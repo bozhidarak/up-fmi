f9144bda37ac694905de26f5255771106aa584bd
bozhidarak <72149764+bozhidarak@users.noreply.github.com>
2020-10-28 23:31:10 +0200

    adding 28.10

---
+++ 28.10/28.10.zad3.cpp
@@ -0,0 +1,37 @@
+// 28.10.zad3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, t1=0, t2=1, nextterm;
+	cin >> n;
+	cout << t1 << ", " << t2 << ", ";
+	nextterm = t1 + t2;
+	while (nextterm <= n)
+	{
+		cout << nextterm<<", ";
+		t1 = t2;
+		t2 = nextterm;
+		nextterm = t1 + t2;
+
+	}
+	
+}
+
+
+
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
